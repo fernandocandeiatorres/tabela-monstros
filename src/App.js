49abe858e92b12c4bd100d16be87b51c70052adf
FernandoTorresDev <fernandotorrescandeia@gmail.com>
2021-12-17 19:15:39 -0300

    changed app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
       monsters: [
         {
@@ -20,14 +21,50 @@ class App extends Component {
           id: "asc3",
         },
       ],
+      value: "",
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    const monsters = this.state.monsters;
+    const newMonsters = [...this.state.monsters];
+    const lastId = parseInt(this.state.monsters[monsters.length - 1].id[3]) + 1;
+
+    newMonsters.push({
+      name: this.state.value,
+      id: `${"asc" + lastId}`,
+    });
+
+    this.setState({ monsters: newMonsters });
+
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => (
           <h1 key={monster.id}> {monster.name} </h1>
         ))}
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Monster:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
