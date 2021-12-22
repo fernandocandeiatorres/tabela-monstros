d9e6ada43ab257294b73a1a9ce9d02aa9c510a51
FernandoTorresDev <fernandotorrescandeia@gmail.com>
2021-12-22 16:22:35 -0300

    Updates

---
+++ precache-manifest.e933b36a11d2c629d9de7e93e87fbacd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3123a927d67ade4c72b5",
+    "url": "/tabela-monstros/static/css/main.7c254dae.chunk.css"
+  },
+  {
+    "revision": "3123a927d67ade4c72b5",
+    "url": "/tabela-monstros/static/js/main.3123a927.chunk.js"
+  },
+  {
+    "revision": "36d575f090ac34ca85dd",
+    "url": "/tabela-monstros/static/js/1.36d575f0.chunk.js"
+  },
+  {
+    "revision": "695ed1d86849a5c6475d",
+    "url": "/tabela-monstros/static/js/2.695ed1d8.chunk.js"
+  },
+  {
+    "revision": "6538b60d1dca9c50ead1",
+    "url": "/tabela-monstros/static/js/runtime~main.6538b60d.js"
+  },
+  {
+    "revision": "f1d9e360883a15c71e20627cd7096f19",
+    "url": "/tabela-monstros/index.html"
+  }
+];
